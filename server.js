1b76e0f85fa168c7b998c456e9bf66dcc6fa2e3e
Zaid Humayun <redixhumayun@gmail.com>
2016-08-12 16:20:48 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		var date = new Date().toString();
+		console.log('Date: '+date+' Request: '+req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function(){
+	console.log('Express Server started');
+});
